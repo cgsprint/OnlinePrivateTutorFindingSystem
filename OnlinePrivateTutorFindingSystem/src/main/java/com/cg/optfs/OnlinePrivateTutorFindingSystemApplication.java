11c18acd3fc51033a061ec482468d7cb562a4950
tejas <tejashatle2@gmail.com>
2020-12-28 11:14:03 +0530

    added pmd plugin

---
+++ OnlinePrivateTutorFindingSystem/src/main/java/com/cg/optfs/OnlinePrivateTutorFindingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cg.optfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePrivateTutorFindingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinePrivateTutorFindingSystemApplication.class, args);
+	}
+
+}
---
+++ OnlinePrivateTutorFindingSystem/src/test/java/com/cg/optfs/OnlinePrivateTutorFindingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.optfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinePrivateTutorFindingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
