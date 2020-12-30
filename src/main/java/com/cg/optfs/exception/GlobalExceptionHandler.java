96169941f829568af7a75e2538ae7f107110e37a
visha <visha@LAPTOP-7B17OPSP>
2020-12-30 13:15:33 +0530

    Booktutor & request

--- src/main/java/com/cg/optfs/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cg/optfs/exception/GlobalExceptionHandler.java
@@ -17,6 +17,33 @@ public class GlobalExceptionHandler {
 		return new ResponseEntity<>(er,HttpStatus.NOT_FOUND);
 	}
 	
+	@ExceptionHandler(RequestNotFoundException.class)
+	public ResponseEntity<ExceptionResponse> handleRequestNotFoundException(RequestNotFoundException ex)
+	{
+		ExceptionResponse er = new ExceptionResponse();
+		er.setErrorCode(HttpStatus.NOT_FOUND.value());
+		er.setErrorMessage(ex.getMessage());
+		return new ResponseEntity<>(er,HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(EbookNotFoundException.class)
+	public ResponseEntity<ExceptionResponse> handleEbookNotFoundException(EbookNotFoundException ex)
+	{
+		ExceptionResponse er = new ExceptionResponse();
+		er.setErrorCode(HttpStatus.NOT_FOUND.value());
+		er.setErrorMessage(ex.getMessage());
+		return new ResponseEntity<>(er,HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(BookedTutorNotFoundException.class)
+	public ResponseEntity<ExceptionResponse> handleBookedTutorNotFoundException(BookedTutorNotFoundException ex)
+	{
+		ExceptionResponse er = new ExceptionResponse();
+		er.setErrorCode(HttpStatus.NOT_FOUND.value());
+		er.setErrorMessage(ex.getMessage());
+		return new ResponseEntity<>(er,HttpStatus.NOT_FOUND);
+	}
+	
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<String> handleException(Exception ex){
 		return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
